45eeb36ab1c49dd5b0672b210334dd0ed289cc66
Oleksandr Voznyi <dfmdeveloper7@DFMMACsedoclocale.local>
2018-07-03 17:41:41 +0300

    Second initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => (
+const Header = (props) => (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Albums</Text>
+            <Text style={styles.textStyle}>{props.headerText}</Text>
         </View>
     );
 
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#F8F8F8'
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 16,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
     },
   textStyle: {
       fontSize: 20
